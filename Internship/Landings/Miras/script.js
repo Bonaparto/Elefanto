634b7fb80dcf6e63a01f079f60679936d827bb8b
Bonapart <mibuchi773@mail.ru>
2021-12-20 18:23:49 +0600

    block8_done

---
+++ Internship/Landings/Miras/script.js
@@ -0,0 +1,8 @@
+function lmao(data, data1) {
+    // console.log(data, data1);
+    // for(info of arguments) console.log(info);
+    return [arguments[3], arguments[arguments.length-1]];
+}
+kek = [];
+kek = lmao(1, 2, 3, 4, 5);
+console.log(kek);
