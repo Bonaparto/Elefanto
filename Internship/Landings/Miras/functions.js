d0f3383f60eea2591cb5acdc5f9ead1b26b142ef
Raiymbek <mibuhic773@mail.ru>
2021-12-27 00:09:29 +0600

    miras_done

--- Internship/Landings/Miras/functions.js
+++ Internship/Landings/Miras/functions.js
@@ -1,23 +1,3 @@
-// first_click1 = true;
-// first_click2 = true;
-// function clean(n) {
-//     inp1 = document.getElementById("input1");
-//     inp2 = document.getElementById("input2");
-//     if(n == '1' && first_click1) {
-//         inp1.value = '';
-//         first_click1 = false;
-//     } else if(n == '2' && first_click2){
-//         inp2.value = '';
-//         first_click2 = false;
-//     } else if(n == '3'){
-//         if(inp1.value == '' || inp2.value == '' || first_click1 || first_click2) {
-//             return
-//         };
-//         document.getElementById("input1").value = '';
-//         document.getElementById("input2").value = '';
-//     }
-// }
-
 let hidden_divs = document.querySelectorAll('.pop-up > div');
 
 for(let i = 0; i < hidden_divs.length; ++i) {
